724ec0270554508a7f709ededa01cda4da539e73
Nathan Crapo <1585130+ntc490@users.noreply.github.com>
2024-05-30 17:15:03 -0600

    Try not using icomplete - do we need it?

--- init.el
+++ init.el
@@ -195,23 +195,23 @@
   :config
   (vertico-mode))
 
-(use-package icomplete
-  :custom
-  (read-file-name-completion-ignore-case t)
-  (read-buffer-completion-ignore-case t)
-  (completion-ignore-case t)
-
-  (completion-category-defaults nil)
-  (completion-category-overrides
-   '((file (styles basic partial-completion))))
-
-  (completion-group t)
-  (completions-group-format
-        (concat
-         (propertize "    " 'face 'completions-group-separator)
-         (propertize " %s " 'face 'completions-group-title)
-         (propertize " " 'face 'completions-group-separator
-                     'display '(space :align-to right)))))
+;;(use-package icomplete
+;;  :custom
+;;  (read-file-name-completion-ignore-case t)
+;;  (read-buffer-completion-ignore-case t)
+;;  (completion-ignore-case t)
+;;
+;;  (completion-category-defaults nil)
+;;  (completion-category-overrides
+;;   '((file (styles basic partial-completion))))
+;;
+;;  (completion-group t)
+;;  (completions-group-format
+;;        (concat
+;;         (propertize "    " 'face 'completions-group-separator)
+;;         (propertize " %s " 'face 'completions-group-title)
+;;         (propertize " " 'face 'completions-group-separator
+;;                     'display '(space :align-to right)))))
 
 (use-package orderless
   :demand t
