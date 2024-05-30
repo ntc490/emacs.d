1cace3ea16015715c1653dadcc6443ce3f08662c
Nathan Crapo <1585130+ntc490@users.noreply.github.com>
2024-05-30 17:15:04 -0600

    fix line number switch in goto-line

--- init.el
+++ init.el
@@ -57,6 +57,16 @@
 
 ;; --------------- General Setup ---------------
 
+(global-set-key [remap goto-line] 'goto-line-with-feedback)
+(defun goto-line-with-feedback ()
+  "Show line numbers temporarily, while prompting for the line number input"
+  (interactive)
+  (unwind-protect
+      (progn
+        (display-line-numbers-mode 1)
+        (goto-line (read-number "Goto line: ")))
+    (display-line-numbers-mode -1)))
+
 ;; Resizing the Emacs frame can be a terribly expensive part of changing the
 ;; font. By inhibiting this, we easily halve startup times with fonts that are
 ;; larger than the system default.
