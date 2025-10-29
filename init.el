b1f64a1ea9a9454a8acef18d535e1a3807d3b7a3
Nathan Crapo <1585130+ntc490@users.noreply.github.com>
2025-10-29 16:29:01 -0600

    Remove benchmark init stuff

--- init.el
+++ init.el
@@ -153,21 +153,6 @@
   :demand t
   :commands esup)
 
-(use-package benchmark-init
-  :demand t
-  :straight (:host github :repo "kekeimiku/benchmark-init-el")
-  :hook (after-init . benchmark-init/deactivate))
-
-(add-hook
- 'emacs-startup-hook
- (lambda ()
-   (message "Emacs ready in %s with %d garbage collections."
-            (format
-             "%.2f seconds"
-             (float-time
-              (time-subtract after-init-time before-init-time)))
-            gcs-done)))
-
 (use-package gcmh
   :demand t
   :config
