89966277fe6934e49d3cdf097f82b1ee33d58808
Nathan Crapo <1585130+ntc490@users.noreply.github.com>
2025-10-29 16:30:19 -0600

    Remove garbage collection tweaking
    Doesn't seem to help startup much and causes a bunch of issues during regular
    use - especially when saving files and the like.

--- init.el
+++ init.el
@@ -93,20 +93,6 @@
             (setq gc-cons-threshold me/gc-cons-threshold
                   gc-cons-percentage 0.1)))
 
-;; max memory available for gc when opening minibuffer
-(defun me/defer-garbage-collection-h ()
-  (setq gc-cons-threshold most-positive-fixnum))
-
-(defun me/restore-garbage-collection-h ()(setq site-run-file nil)
-  ;; Defer it so that commands launched immediately after will enjoy the
-  ;; benefits.
-  (run-at-time
-   1 nil (lambda () (setq gc-cons-threshold me/gc-cons-threshold))))
-
-(add-hook 'minibuffer-setup-hook #'me/defer-garbage-collection-h)
-(add-hook 'minibuffer-exit-hook #'me/restore-garbage-collection-h)
-(setq garbage-collection-messages t)
-
 (defvar me/-file-name-handler-alist file-name-handler-alist)
 (setq file-name-handler-alist nil)
 (add-hook 'emacs-startup-hook
